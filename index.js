380bddfd65017c40650bfcef57f77c82d347ae39
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-05-08 21:21:13 -0400

    Changing the logic so the user is presented with the engineer/intern/finish options instead of add new member yes/no

--- index.js
+++ index.js
@@ -1,48 +1,92 @@
-const fs = require ('fs');
 const inquirer = require ('inquirer');
+const fs = require ('fs');
 const Manager = require ('./lib/manager');
 const Engineer = require ('./lib/engineer');
 const Intern = require ('./lib/intern');
-// const { async } = require('rxjs');
 var employee = 'Manager';
-const employeeQuestions = [
+
+const managerQuestions = [
     {
         type:    'input',
-        message: `Enter the employee's name:`,
+        message: `Enter the manager's name:`,
         name:    'name',
     },
     {
         type:    'input',
-        message: `Enter the employee's ID:`,
+        message: `Enter the manager's ID:`,
         name:    'id',
     },
     {
         type:    'input',
-        message: `Enter the employee's email:`,
+        message: `Enter the manager's email:`,
         name:    'email',
     },
-];
-
-const managerQuestions = [
     {
         type:    'input',
         message: `Enter the manager's office number:`,
         name:    'office',
     },
 ];
-const addMEmber = [
+
+const engineerQuestions = [
     {
-        type:    'list',
-        message: `Would you like to add another member to the team?`,
-        name:    'add',
-        choices: ['Yes','No']
+        type:    'input',
+        message: `Enter the engineer's name:`,
+        name:    'name',
     },
     {
+        type:    'input',
+        message: `Enter the engineer's ID:`,
+        name:    'id',
+    },
+    {
+        type:    'input',
+        message: `Enter the engineer's email:`,
+        name:    'email',
+    },
+    {
+        type:    'input',
+        message: `Enter the engineer's github username:`,
+        name:    'github',
+    },
+];
+
+const internQuestions = [
+    {
+        type:    'input',
+        message: `Enter the intern's name:`,
+        name:    'name',
+    },
+    {
+        type:    'input',
+        message: `Enter the intern's ID:`,
+        name:    'id',
+    },
+    {
+        type:    'input',
+        message: `Enter the intern's email:`,
+        name:    'email',
+    },
+    {
+        type:    'input',
+        message: `Enter the intern's school:`,
+        name:    'school',
+    },
+];
+
+const addMEmber = [
+    // {
+    //     type:    'list',
+    //     message: `Would you like to add another member to the team?`,
+    //     name:    'add',
+    //     choices: ['Yes','No']
+    // },
+    {
         type:    'list',
-        message: `What position does the member hold?`,
-        name:    'position',
-        choices: ['Engineer','Intern'],
-        when: (answers) => answers.add === 'Yes'
+        message: `Add another team member`,
+        name:    'option',
+        choices: ['Engineer','Intern','Exit'],
+        // when: (answers) => answers.add === 'Yes'
     },
 ];
 function init (){
@@ -51,46 +95,47 @@ function init (){
 }
 
 createEmployee = (employee) => {
-    (employee === 'Manager') ? console.info(`Creating a ${employee}`) : console.info(`Creating an ${employee}`);
-    inquirer
-    .prompt (employeeQuestions)
-    .then((response) => {
-        switch (employee){
-            case 'Manager':
-                const manager = new Manager(response.name,response.id,response.email,'');
-                inquirer
-                .prompt (managerQuestions)
-                .then((response) => {
-                    manager.officeNumber = response.office;
+    switch (employee){
+        case 'Manager':
+            console.info(`Adding the team Manager`);
+            inquirer.prompt (managerQuestions)
+            .then((answers) => {
+                    const manager = new Manager(answers.name,answers.id,answers.email,answers.office);
                     console.log(manager);
-                })
-                .then(() => addAnotherMember());
-                break;
-            case 'Engineer':
-                break;
-            case 'Intern':
-                break;
-            default:
-                console.log('sorry');
-                break;
-        }
-    });
+            })
+            .then(() => addAnotherMember());
+            break;
+        case 'Engineer':
+            console.info(`Adding a team engineer`);
+            inquirer.prompt (engineerQuestions)
+            .then((answers) => {
+                    const engineer = new Engineer(answers.name,answers.id,answers.email,answers.github);
+                    console.log(engineer);
+            })
+            .then(() => addAnotherMember());
+            break;
+        case 'Intern':
+            console.info(`Adding a team Intern`);
+            inquirer.prompt (internQuestions)
+            .then((answers) => {
+                    const intern = new Intern(answers.name,answers.id,answers.email,answers.intern);
+                    console.log(intern);
+            })
+            .then(() => addAnotherMember());
+            break;
+        default:
+            console.log('sorry');
+            break;
+    }
 }
 
 
 addAnotherMember = () =>{
-    inquirer
-    .prompt(addMEmber)
+    inquirer.prompt(addMEmber)
     .then((response) =>{
-        switch (response.add){
-            case 'Yes':
-                employee = response.position;
-                createEmployee(employee);
-                break;
-            case 'No':
-                break;
-        }
-    });
+            employee = response.option;
+            createEmployee(employee);
+        });
 }
 
 init();
